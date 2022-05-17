42a7c7142841d79cb9cf1642540f63a3786b2d97
zhaokun <zhaokun@tydic.com>
2022-05-17 17:54:14 +0800

    引入bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 import './assets/css/index.css'
