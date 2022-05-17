44a47a03b442d704f9039cc919d47537c9b1fe7f
zhaokun <zhaokun@tydic.com>
2022-05-17 18:46:28 +0800

    去除bootstrap.js的引用

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,11 +4,11 @@
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
         <g-link to='/'>我的博客</g-link>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button @click="toggleShow" class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           菜单
           <i class="fas fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div class="collapse navbar-collapse" :class='{show: showNav}' id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
               <g-link class="nav-link" to='/'>首页</g-link>
@@ -52,6 +52,23 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Layout',
+  data () {
+    return {
+      showNav: false,
+    }
+    
+  },
+  methods: {
+    toggleShow () {
+      this.showNav = !this.showNav
+    }
+  }
+}
+</script>
 
 <style>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 import './assets/css/index.css'
